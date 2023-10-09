e0abba5a1abdb1a72e5ebdd213c728b482b9b949
maxmusau <musaumaxwell@gmail.com>
2023-10-09 11:52:28 +0300

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import  *
+import pymysql #connect to the database
 # create the app
 app=Flask(__name__)
 @app.route("/")
@@ -37,19 +38,44 @@ def Signup():
 def Users():
     return users
 
-
+connection=pymysql.connect(host="localhost",user="root",database="alpha",password="")
 # import pymysql
-import pymysql #connect to the database
+
 @app.route("/employees",methods=['GET','POST'] )
 def Employees():
     if request.method == 'POST':
         # TODO
-        pass
+        emp_name=request.form['emp_name']
+        salary=request.form['salary']
+        dept_id=request.form['dept_id']
+        # check if user has provided all the details
+        if not emp_name or not salary or not dept_id:
+            return render_template("employees.html",message="Please fill in all the details")
+        # create the cursor function
+        # cursor function -used to execute the sql query
+        cursor=connection.cursor()
+        # define the sql
+        sql='insert into employees (emp_name,salary,dept_id) values(%s,%s,%s)' 
+        # execute sql
+        cursor.execute(sql,(emp_name,salary,dept_id))
+        # commit
+        connection.commit()
+        # return statement
+        return render_template("employees.html",message="Employee saved successfully")
+        # pass
     else:
         return render_template("employees.html")
            
          
-    
+# check database connection    # 
+@app.route("/check_database")
+def Check_database():
+    try:   
+        connection=pymysql.connect(host="localhost",user="root",database="alpha",password="")
+        print("Connection successful")
+        return "Connection successful"
+    except:
+        return "Connection failed"
 
 
 
